04d5be6f802d931ceb7a36725281c5ff1144b799
Paul Krizak <paul.krizak@gmail.com>
2022-11-27 16:37:51 -0800

    finalize keyboard tester

--- assembler/keyboard_test/keyboard_test.asm
+++ assembler/keyboard_test/keyboard_test.asm
@@ -5,13 +5,15 @@ NOP
 # Clears the screen, displays a simple '>' prompt with
 # blinking cursor, and prints characters as they are
 # typed on the keyboard.
+#
+# The C register is used as a global var and stores the current
+# cursor location.  The D register stores the same location but
+# in the color region of the display memory.
 
-# Install IRQ handlers.  The IRQ0 (keyboard) handler
-# starts off with a "wait for initialization" handler,
-# which we swap out for proper keyboard handling once
-# the keyboard controller is initialized.
-ST16    %IRQ0addr%  .irq0_init
-ST16    %IRQ1addr%  .noirq
+# Install default IRQ handlers.
+MASKINT
+ST16    %IRQ0addr%  .noirq
+ST16    %IRQ1addr%  .irq1
 ST16    %IRQ2addr%  .noirq
 ST16    %IRQ3addr%  .noirq
 ST16    %IRQ4addr%  .noirq
@@ -19,8 +21,38 @@ ST16    %IRQ5addr%  .noirq
 ST16    %IRQ6addr%  .noirq
 ST16    %IRQ7addr%  .noirq
 
-# Enable interrupts
-UMASKINT
+# Clear the screen
+LDI_AH  0x00     # char to clear screen with
+LDI_AL  %white%
+CALL :clear_screen
+
+# Put the initialization string on screen
+LDI_C   .data0
+LDI_D   %display_chars%
+CALL :strcpy
+
+# The ATTiny takes ~75ms to initialize the keyboard.
+# We sleep for >100ms to give it plenty of time.
+LDI_A   0xffff
+.kb_wait_loop
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+NOP                     # 2 clocks
+ALUOP_AL %A-1%+%AL%     # 4 clocks # 32 total
+JNZ .kb_wait_loop
+ALUOP_AH %A-1%+%AH%
+JNZ .kb_wait_loop
 
 # Clear the screen
 LDI_AH  0x00     # char to clear screen with
@@ -29,53 +61,53 @@ CALL :clear_screen
 
 # Track cursor position in regs C (char) and D (color)
 LDI_C   %display_chars%
-LDI_TD  '>'     # prompt character
-STA_C_TD        # Write '>' to first byte
-INCR_C          # move cursor to the right
+LDI_TD  '>'             # prompt character
+STA_C_TD                # Write '>' to first byte
+INCR_C                  # move cursor to the right
 LDI_D   %display_color%+1
 LDI_TD  %white%+%blink%+%cursor%
-STA_D_TD        # enable blinking cursor
+STA_D_TD                # enable blinking cursor
 
-###
+
+#######
 # Main loop
-###
+#######
+LD_TD  %kb_key%         # clear any pending KB interrupt
+UMASKINT                # Enable KB interrupt
 .main_loop
 JMP .main_loop
+HLT
 
 #######
-# IRQ handlers
+# IRQ handler
 #######
+.irq1
+ALUOP_PUSH %A%+%AL%                     # we're going to use AL and BL to inspect the
+ALUOP_PUSH %B%+%BL%                     # KB flags register
 
-# When the KB controller first comes out of reset, it takes a few
-# seconds to initialize and become responsive to commands.  When
-# it has finished initializing, it triggers an interrupt.  This
-# handler captures that interrupt, clears it, configues the keyboard
-# controller how we like, and then installs the actual interrupt
-# handler.
-.irq0_init
-# Clear the startup interrupt
-STS     %kb_kbctrl% %kb_kbctrl_INTCLEAR%
-# Set up the keyboard - interrupt on key make events,
-# and clear interrupts on reads to %kb_key%
-STS     %kb_config% %kb_config_INTMAKE%+%kb_config_INTCLR_READ%
-# Configure the real keyboard handler
-ST16    %IRQ0addr%  .irq0
+LD_AL %kb_keyflags%
+LDI_BL %kb_keyflag_BREAK%
+ALUOP_FLAGS %A&B%+%AL%+%BL%
+JNZ .endirq1                            # Skip the rest of the IRQ handler if this
+                                        # was a break event (nonzero)
+
+LDI_TD  %white%                         # disable blinking cursor in current position
+STA_D_TD
+INCR_D                                  # move the color position to the right
+
+LD_TD  %kb_key%                         # fetch the keystroke into TD
+STA_C_TD                                # write TD to the current cursor position
+INCR_C                                  # move char cursor to the right
+
+LDI_TD  %white%+%blink%+%cursor%        # enable blinking cursor in next position
+STA_D_TD
+.endirq1
+POP_BL                                  # restore registers we pushed at the
+POP_AL                                  # beginning of the IRQ handler
 RETI
 
-# Keyboard event handler
-.irq0
-LDS_TD  %kb_key%        # fetch the key into TD
-STA_C_TD                # write TD to the current cursor position
-INCR_C                  # move char cursor to the right
-LDI_TD  %white%
-STA_D_TD                # disable blinking cursor in current position
-INCR_D
-LDI_TD  %white%+%blink%+%cursor%
-STA_D_TD                # enable blinking cursor in new position
-RETI
-
-# Disabled IRQ handlers simply return
-# without doing anything
+# Disabled IRQ handlers simply return without doing anything
 .noirq
 RETI
 
+.data0 "Waiting for KB controller...\0"
