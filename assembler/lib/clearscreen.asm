0742c6268c4873a8604eb59b13d33b8772fc8afa
Paul Krizak <paul.krizak@gmail.com>
2023-08-12 21:25:29 -0700

    Use memfill command for clearing screen

--- assembler/lib/clearscreen.asm
+++ assembler/lib/clearscreen.asm
@@ -9,39 +9,22 @@
 #   AL: color to fill screen with
 
 :clear_screen
-
-ALUOP_PUSH %B%+%BH%
-ALUOP_PUSH %B%+%BL%
 PUSH_CH
 PUSH_CL
-PUSH_DH
-PUSH_DL
+ALUOP_PUSH %A%+%AL%             # save color byte for later
 
-LDI_B   0x0f00                  # 64x60 chars to clear
-LDI_C   %display_chars%         # framebuffer chars
-LDI_D   %display_color%         # framebuffer color
+LDI_C %display_chars%
+LDI_AL 29                       # 30 128 byte segments -> 60 lines
+CALL :memfill_segments
 
-.loop
-ALUOP_ADDR_C %A%+%AH%           # store AH into RAM@C (char)
-INCR_C
-ALUOP_ADDR_D %A%+%AL%           # store AL into RAM@D (color)
-INCR_D
+PEEK_AH                         # Put the color char into AH
+LDI_C %display_color%
+LDI_AL 29                       # 30 128 byte segments -> 60 lines
+CALL :memfill_segments
 
-ALUOP_BL    %B-1%+%BL%          # decrement BL
-JNO .decr16_done                # if this didn't overflow BL, don't decrement BH
-ALUOP_BH    %B-1%+%BH%          # decrement BH on overflow
-.decr16_done
-ALUOP_FLAGS %B%+%BL%
-JNZ .loop                       # continue looping if BL is non-zero
-ALUOP_FLAGS %B%+%BH%
-JNZ .loop                       # continue looping if BH is non-zero
-
-POP_DL
-POP_DH
+POP_AL
 POP_CL
 POP_CH
-POP_BL
-POP_BH
 
 RET
 
