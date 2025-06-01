db3c9fb83366d826a325c094997d1e4b6cd54af8
Paul Krizak <paul.krizak@gmail.com>
2025-06-01 11:27:27 -0700

    Set up new malloc range in extended memory

--- assembler/os_shell/10-malloc_init.asm
+++ assembler/os_shell/10-malloc_init.asm
@@ -2,15 +2,31 @@
 
 :boot_malloc_init
 LDI_A 0x6000
-LDI_BL 159   # 160 segments = 20KiB
+LDI_BL 79    # 80 segments = 10KiB
 CALL :malloc_init
 LDI_C .malloc_init_banner
-LDI_A 0xb000-0x6000
+LDI_A 0x8800-0x6000
+CALL :heap_push_A
+LDI_A 0x87ff
+CALL :heap_push_AL
+CALL :heap_push_AH
+LDI_A 0x6000
+CALL :heap_push_AL
+CALL :heap_push_AH
+CALL :printf
+
+LDI_A 0x8800
+LDI_BL 80    # 80 segments = 10KiB
+CALL :new_malloc_init
+LDI_C .new_malloc_banner
+CALL :print
+LDI_C .malloc_init_banner
+LDI_A 0xb000-0x8800
 CALL :heap_push_A
 LDI_A 0xafff
 CALL :heap_push_AL
 CALL :heap_push_AH
-LDI_A 0x6000
+LDI_A 0x8800
 CALL :heap_push_AL
 CALL :heap_push_AH
 CALL :printf
@@ -20,3 +36,4 @@ CALL :extmalloc_init
 RET
 
 .malloc_init_banner "Malloc range 0x%x%x-0x%x%x (%U bytes)\n\0"
+.new_malloc_banner "New \0"
