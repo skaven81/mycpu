213b2e699b4c7320e9352db51d38077a38e8899b
Paul Krizak <paul.krizak@gmail.com>
2026-03-01 11:51:45 -0800

    Add calloc_segments

--- os/bios/lib/malloc.h
+++ os/bios/lib/malloc.h
@@ -2,3 +2,4 @@ extern void free(void *ptr);
 extern void *malloc_blocks(uint8_t blocks);
 extern void *calloc_blocks(uint8_t blocks);
 extern void *malloc_segments(uint8_t segments);
+extern void *calloc_segments(uint8_t segments);
